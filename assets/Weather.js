bdf93778c9e830932b34492c75b81f1ffb4fe265
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2022-09-04 01:06:43 -0500

    CSS alignment and html

--- assets/Weather.js
+++ assets/Weather.js
@@ -1,4 +1,25 @@
-var url= "https://api.openweathermap.org/data/2.5/weather?lat=97.7431°W&lon=30.2672°N&appid={1554dda1921875abae39b08caa1f2b95"
 
-//,
-fetch (url) 
+var searchButton = document.getElementById('search-button');
+
+
+
+function getApi() {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=houston&appid=1554dda1921875abae39b08caa1f2b95";
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+          console.log('data');}
+      
+        //     for (var i=0; i<data.length; i++);
+        //     var currentConditions= document.createElement('p');
+        //     var futureConditions=document.createElement('p');
+
+        //     currentConditions.textContent=data[i].weather.main.description.icon;
+        //     futureConditions=textContent=data[i].weather.main.description.icon
+
+        // })
+)};
+
+
