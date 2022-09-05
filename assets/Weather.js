fa224bf7199e6245deaeef3b76b6d91aa9fcb556
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2022-09-04 20:25:46 -0500

    html and CSS

--- assets/Weather.js
+++ assets/Weather.js
@@ -1,25 +1,29 @@
 
 var searchButton = document.getElementById('search-button');
-
+var largeWeatherCard = document.getElementById('sameDayLgDisplay');
 
 
 function getApi() {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=houston&appid=1554dda1921875abae39b08caa1f2b95";
+
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat=32.7831&lon=-96.7836&appid=a8032d6652322cc8dd1fcdb66cf0f2b8"
+
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-          console.log('data');}
-      
-        //     for (var i=0; i<data.length; i++);
-        //     var currentConditions= document.createElement('p');
-        //     var futureConditions=document.createElement('p');
+            console.log(data);
 
-        //     currentConditions.textContent=data[i].weather.main.description.icon;
-        //     futureConditions=textContent=data[i].weather.main.description.icon
-
-        // })
-)};
+            for (var i = 0; i < data.length; i++);
+            var currentConditions = document.createElement('p');
+            var futureConditions = document.createElement('p');
 
 
+            currentConditions.textContent = data[i].weather.main.description.icon;
+            futureConditions.textContent = data[i].weather.main.description.icon;
+
+        });
+}
+
+
+searchButton.addEventListener('click', getApi);
